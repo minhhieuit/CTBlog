f6138c40bb04e696662799fe4b0a805fc7d11852
hieuit <info.hieuit@gmail.com>
2017-07-19 01:39:17 +0700

    Add project files.

---
+++ CTBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CTBlog.Startup))]
+namespace CTBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
